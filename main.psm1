3237b6740b7065ec73284b9851dae2eb813be83c
jeremy woo <kepungmoin@hotmail.com>
2019-07-03 19:59:45 +1200

    added deploment

--- main.psm1
+++ main.psm1
@@ -33,10 +33,19 @@ function CreateFunctionApp() {
 
 function DeployAppFunction($functionAppName, $resourceGroup, $filePath) {
 
-    $apiUrl = "https://$functionAppName.scm.azurewebsites.net/api/zipdeploy"
-    $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $username, $password)))
-    $userAgent = "powershell/1.0"
-    Invoke-RestMethod -Uri $apiUrl -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -UserAgent $userAgent -Method POST -InFile $filePath -ContentType "multipart/form-data"
+    $siteName = 'claimsubmissionfunction'
+    $ResourceGroupName = 'myResourceGroup'
+    $PublishingProfile = [xml](Get-AzureRmWebAppPublishingProfile -ResourceGroupName $ResourceGroupName -Name $siteName)
+    
+    $Username = (Select-Xml -Xml $PublishingProfile -XPath "//publishData/publishProfile[contains(@profileName,'Web Deploy')]/@userName").Node.Value
+    $Password = (Select-Xml -Xml $PublishingProfile -XPath "//publishData/publishProfile[contains(@profileName,'Web Deploy')]/@userPWD").Node.Value
+    
+    $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $Username,$Password)))
+    
+
+    $apiUrl = "https://$siteName.scm.azurewebsites.net/api/zipdeploy";
+
+    Invoke-RestMethod -Uri $apiUrl -InFile $filePath -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -Method Post -ContentType "multipart/form-date";
 
 }
 
